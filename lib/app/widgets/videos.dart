cad932fb6825c3e7a8e4dbb0c213058622422994
Joseph-W-E <joey.w.elliott@gmail.com>
2024-01-13 15:29:41 -0600

    Give videos a progress bar, let you swipe to seek

--- lib/app/widgets/videos.dart
+++ lib/app/widgets/videos.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lurkur/app/blocs/preference_cubit.dart';
+import 'package:lurkur/app/blocs/theme_cubit.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart' as vp;
 import 'package:visibility_detector/visibility_detector.dart';
@@ -93,19 +95,44 @@ class _VideoPlayerState extends State<_VideoPlayer> {
             Positioned.fill(
               child: vp.VideoPlayer(_videoController),
             ),
-            GestureDetector(
-              onTap: () {
-                if (!_videoController.value.isInitialized) return;
-                _videoController.value.isPlaying
-                    ? _videoController.pause()
-                    : _videoController.play();
-              },
-              child: Container(
-                color: Colors.transparent,
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ValueListenableBuilder(
+                  valueListenable: _videoController,
+                  builder: (context, value, widget) {
+                    return _ProgressBar(
+                      progress: (value.position.inMilliseconds /
+                              value.duration.inMilliseconds)
+                          .clamp(0.0, 1.0),
+                    );
+                  },
+                ),
               ),
+            ),
+            _Glass(
+              onTap: _playOrPause,
+              onSeek: _seekBy,
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _playOrPause() {
+    if (!_videoController.value.isInitialized) return;
+    _videoController.value.isPlaying
+        ? _videoController.pause()
+        : _videoController.play();
+  }
+
+  void _seekBy(double percent) {
+    final ms = _videoController.value.position.inMilliseconds;
+    final totalMs = _videoController.value.duration.inMilliseconds;
+    _videoController.seekTo(
+      Duration(
+        milliseconds: (ms + totalMs * percent).round().clamp(0, totalMs),
       ),
     );
   }
@@ -117,5 +144,67 @@ class _VideoPlayerState extends State<_VideoPlayer> {
     } else {
       _videoController.pause();
     }
+  }
+}
+
+class _ProgressBar extends StatelessWidget {
+  const _ProgressBar({
+    required this.progress,
+  });
+
+  final double progress; // 0.0 to 1.0
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Row(
+          children: [
+            if (0 < progress)
+              AnimatedContainer(
+                duration: 0.2.seconds,
+                height: 4,
+                width: (constraints.maxWidth * progress).clamp(
+                  0,
+                  constraints.maxWidth - 1,
+                ),
+                color: context.colorScheme.outline,
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+              )
+                  .animate(
+                    onComplete: (c) => c.loop(period: 4.seconds),
+                  )
+                  .shimmer(),
+            const Spacer(),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _Glass extends StatelessWidget {
+  const _Glass({
+    required this.onTap,
+    required this.onSeek,
+  });
+
+  final void Function() onTap;
+  final void Function(double percent) onSeek;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      onHorizontalDragUpdate: (details) {
+        onSeek(
+          details.delta.dx > 0 ? 0.025 : -0.025,
+        );
+      },
+      child: Container(
+        color: Colors.transparent,
+      ),
+    );
   }
 }
